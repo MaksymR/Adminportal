5acc788808123e5121fd91aa73b7a39a0ee3eb05
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-24 20:41:38 +0300

    created the book controller for "addBook.html" in the "com.adminportal.controller.BookController.java"

---
+++ src/main/java/com/adminportal/controller/BookController.java
@@ -0,0 +1,23 @@
+package com.adminportal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.adminportal.domain.Book;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    @RequestMapping("/add")
+    public String addBook(Model model) {
+        /*
+         * create a new instance
+         */
+        Book book = new Book();
+        model.addAttribute("book", book);
+        return "addBook";
+    }
+
+}
