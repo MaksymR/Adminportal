9936b13a54562c243f5390b1fe8bfc5c46bc1ccf
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-25 11:14:14 +0300

    added the "GET"-method for "add"-mapping and "addBookPost" with "POST"-method in the "com.adminportal.controller.BookController.java"

--- src/main/java/com/adminportal/controller/BookController.java
+++ src/main/java/com/adminportal/controller/BookController.java
@@ -1,22 +1,36 @@
 package com.adminportal.controller;
 
+import com.adminportal.domain.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.adminportal.domain.Book;
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 @Controller
 @RequestMapping("/book")
 public class BookController {
 
-    @RequestMapping("/add")
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String addBook(Model model) {
         /*
          * create a new instance
          */
         Book book = new Book();
         model.addAttribute("book", book);
+        return "addBook";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addBookPost(@ModelAttribute("book") Book book, HttpServletRequest request) {
+        bookService.save(book);
+
         return "addBook";
     }
 
