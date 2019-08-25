3802fd52685345ca932e0731cb73e9fa65b5fd06
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-25 14:08:11 +0300

    implemented the "findAll()"-method of interface "com.adminportal.service.BookService.java" for finding all books in the "com.adminportal.service.impl.BookControllerImpl.java"

--- src/main/java/com/adminportal/service/impl/BookServiceImpl.java
+++ src/main/java/com/adminportal/service/impl/BookServiceImpl.java
@@ -1,11 +1,12 @@
 package com.adminportal.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.adminportal.domain.Book;
 import com.adminportal.repository.BookRepository;
 import com.adminportal.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookServiceImpl implements BookService {
@@ -13,9 +14,16 @@ public class BookServiceImpl implements BookService {
     @Autowired
     private BookRepository bookRepository;
 
+    @Override
     public Book save(Book book) {
 
         return bookRepository.save(book);
 
     }
+
+    @Override
+    public List<Book> findAll() {
+        return (List<Book>) bookRepository.findAll();
+    }
+
 }
