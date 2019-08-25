8a785630317061b4df9e8dce0bdd006036f298c2
Maksym Riaboshapka <makmr11@gmail.com>
2019-08-25 11:43:54 +0300

    created the "save"-method in the "com.adminportal.service.impl.BookServiceImpl.java"

--- src/main/java/com/adminportal/service/impl/BookServiceImpl.java
+++ src/main/java/com/adminportal/service/impl/BookServiceImpl.java
@@ -1,9 +1,11 @@
 package com.adminportal.service.impl;
 
-import com.adminportal.domain.Book;
-import com.adminportal.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.adminportal.domain.Book;
+import com.adminportal.repository.BookRepository;
+import com.adminportal.service.BookService;
 
 @Service
 public class BookServiceImpl implements BookService {
@@ -11,9 +13,9 @@ public class BookServiceImpl implements BookService {
     @Autowired
     private BookRepository bookRepository;
 
-    @Override
     public Book save(Book book) {
-        return null;
-    }
 
+        return bookRepository.save(book);
+
+    }
 }
